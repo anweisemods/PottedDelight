b8b3e83177d8e6f093a6444b6ade5fa6b0dea041
Angelo <60273047+anweisen@users.noreply.github.com>
2025-03-17 21:07:04 +0100

    feat: add common resources

---
+++ common/src/main/java/net/anweisen/potteddelight/PottedDelightCommons.java
@@ -0,0 +1,12 @@
+package net.anweisen.potteddelight;
+
+/**
+ * @author anweisen | https://github.com/anweisen
+ * @since 1.0
+ */
+public class PottedDelightCommons {
+
+  public static final String MOD_NAME = "PottedDelight";
+  public static final String MOD_ID = "potteddelight";
+
+}
